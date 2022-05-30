256831ebb531b248fd3031a0951901189f57482e
liliana <karlaliliana.03@gmail.com>
2022-05-30 13:56:39 -0500

    Second Version Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { HeroesModule } from './heroes/heroes.module';
